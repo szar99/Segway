49efa5783cd09760da1ea4691824d93dd24a7984
szar99 <maciekszarek@onet.pl>
2023-11-24 10:00:35 +0100

    Integration PixyCam and 2dof gimbal first version running

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Segway
+
+## Project plan
+
+Project is devided into three main stages. 
+- First stage contains of small simple differential robot project that will use Pixy camera for navigation and two motors that, with the help of speed differentiation, will allow you to move in the right direction.
+- Second stage consists of a segway design that can independently maintain balance and move in the desired direction.
+- The third stage involves combining the two previous concepts and designing a segway that will use a camera to navigate and track the object.
+
+### Stage 1 - differential robot
+
+Milestones:
+- Pixy-Cam driver
+- Hardware 
+- CAD design
+- Building
+- Software
+
+### Stage 2 - simple segway
+
+### Stage 3 - segway with pixy cam
--- main.cpp
+++ main.cpp
@@ -1,10 +1,53 @@
 #include "mbed.h"
+#include "PM2_Drivers.h"
 
-// main() runs in its own thread in the OS
+
+PixyCam pixy(PC_12, PD_2, 230400);
+
 int main()
 {
-    while (true) {
 
+
+    DigitalIn mechanical_button(PC_5); // create DigitalIn object to evaluate extra mechanical button, you need to specify the mode for proper usage, see below
+    mechanical_button.mode(PullUp);    // set pullup mode: sets pullup between pin and 3.3 V, so that there is a defined potential
+    while (true)
+    {
+
+        if (mechanical_button.read()) {
+            pixy.setServos(0,0);
+            ThisThread::sleep_for(2s);
+            pixy.setServos(500,500);
+            ThisThread::sleep_for(2s);
+            pixy.setServos(1000,1000);
+            ThisThread::sleep_for(2s);
+            pixy.setServos(500,500);
+        }
+        ThisThread::sleep_for(100ms);
     }
 }
 
+
+//Script for gimbal assembly - servos positioning
+/*    
+int main()
+{
+
+
+    DigitalIn mechanical_button(PC_5); // create DigitalIn object to evaluate extra mechanical button, you need to specify the mode for proper usage, see below
+    mechanical_button.mode(PullUp);    // set pullup mode: sets pullup between pin and 3.3 V, so that there is a defined potential
+    while (true)
+    {
+
+        if (mechanical_button.read()) {
+            pixy.setServos(0,0);
+            ThisThread::sleep_for(2s);
+            pixy.setServos(500,500);
+            ThisThread::sleep_for(2s);
+            pixy.setServos(1000,1000);
+            ThisThread::sleep_for(2s);
+            pixy.setServos(500,500);
+        }
+        ThisThread::sleep_for(100ms);
+    }
+}
+*/
